b8154a7a3c91e683489f750314313e56f6aa2465
abu07939 <abufaizil407@gmail.com>
2026-01-21 14:44:51 +0530

    added pattern 11

--- pattern11.java
+++ pattern11.java
@@ -1,3 +1,14 @@
 public class pattern11 {
-    
-}
+    public static void main(String args[]){
+        pattern11(5);
+    }
+    static void pattern11(int n){
+        for(int row=0;row<=n;row++){
+            for(int space=0;space<=n-row;space++){
+                System.out.print(" ");
+            }
+            for(int col=0;col<=2*row;col++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }}
